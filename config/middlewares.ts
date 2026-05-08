f27e75ca4f313a91ddb82f902223795134a77a9f
Your Name <somanimuskan2705@gmail.com>
2026-05-08 18:27:30 +0530

    Prepare CMS for hosted deployment

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,10 +1,21 @@
 import type { Core } from '@strapi/strapi';
 
-const config: Core.Config.Middlewares = [
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Middlewares => [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: env
+        .array('CORS_ORIGIN', ['http://localhost:5500', 'http://127.0.0.1:5500'])
+        .map((origin) => origin.trim())
+        .filter(Boolean),
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
